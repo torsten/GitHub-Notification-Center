f6fd51d3e2880dd744850963f3e39ad1344c5a71
Lars Schneider <larsxschneider@gmail.com>
2012-05-04 13:48:36 +0200

    Add app skeleton.

---
+++ Source/Classes/TLAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  TLAppDelegate.h
+//  GitHubNotificationCenter
+//
+//  Created by Lars Schneider on 5/4/12.
+//  Copyright (c) 2012 6Wunderkinder. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Source/Classes/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GitHubNotificationCenter
+//
+//  Created by Lars Schneider on 5/4/12.
+//  Copyright (c) 2012 6Wunderkinder. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
